8b0dd52262b607e88d24273697e6e4de8b33ce35
sudharshanan <sudharshananv56@gmail.com>
2024-11-17 00:07:08 +0530

    amazon homepage and prodduct page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'HomePage.dart';
+import 'ProductPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Amazonhomepage(),
+      home: Productpage(),
     );
   }
 }
